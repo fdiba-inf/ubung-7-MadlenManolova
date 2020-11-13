e8c5029637e8f09f727671ff13f2201e4fd0da3d
MadlenManolova <madllymanolova@gmail.com>
2020-11-13 22:29:24 +0000

    NumberCounter

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -14,9 +14,22 @@ public class Triangle {
         }
 
         // Print the bottom part of the triangle that is under the longest line
+        for (int line = n-1; line >= 1; line--) {
+            printLine(1, line);
+        }
     }
 
     public static void printLine(int start, int end) {
-        // Implement method
+      // Implement method
+      for (; start<=end; start++){
+       System.out.print(start);
+       if(start!=end){
+         System.out.print (" ");
+
+       }
+      }
+        
+      System.out.println();
+
     }
 }
